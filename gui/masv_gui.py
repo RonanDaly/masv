6e9c5f61addeaad21596c97aa3dd9c1c74fec2b0
CorwinAnsley <coreyansley@gmail.com>
2025-08-11 05:06:32 +0100

    working on gui

--- gui/masv_gui.py
+++ gui/masv_gui.py
@@ -1,5 +1,40 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog
+import pandas as pd
+
+from PyQt6.QtCore import QAbstractTableModel, Qt
+from PyQt6.QtGui import QBrush, QColor
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QTableView, QWidget
+
+# Model to contain masv data
+class MasvTableModel(QAbstractTableModel):
+    def __init__(self, data, col_highlight = None, row_highlight = None):
+        super().__init__()
+        self._data = data
+
+        self.col_highlight = col_highlight  
+        self.row_highlight = row_highlight
+        self.colors = dict()
+
+    def data(self, index, role, col_highlight = None, row_highlight = None):
+        if role == Qt.ItemDataRole.DisplayRole:
+            return self._data[index.row()][index.column()]
+        elif role == Qt.ItemDataRole.BackgroundRole and index.column() == self.col_highlight:
+            # See below for the data structure.
+            if index.row() == self.row_highlight or self.row_highlight == None:
+                return QColor('red')
+        elif role == Qt.ItemDataRole.BackgroundRole and index.row() == self.row_highlight:
+            # See below for the data structure.
+            if index.column() == self.col_highlight or self.col_highlight == None:
+                return QColor('red')
+
+    # returns num of rows
+    def rowCount(self, index):
+        return len(self._data)
+    
+    # returns num of columns (assumes all rows of equal length)
+    def columnCount(self, index):
+        return len(self._data[0])
+    
 
 # Subclass QMainWindow to customize your application's main window
 class MainMasvWindow(QMainWindow):
@@ -14,13 +49,22 @@ class MainMasvWindow(QMainWindow):
         select_file_button.clicked.connect(self.select_file_click)
 
         # Table to display data
+        self.table = QTableView()
 
         # Set the central widget of the Window.
         self.setCentralWidget(select_file_button)
     
     def select_file_click(self):
-            masv_file = QFileDialog.getOpenFileName(self, self.tr("Select MASV file"), "", self.tr("Masv file (*.tsv)"))
-            print(masv_file)
+            masv_file_select = QFileDialog.getOpenFileName(self, self.tr("Select MASV file"), "", self.tr("Masv file (*.tsv)"))
+            filepath = masv_file_select[0]
+
+            masv_df = pd.read_csv(filepath, sep='\t')
+
+            self.model = MasvTableModel(masv_df.values.tolist(), col_highlight = 2, row_highlight = 2)
+            self.table.setModel(self.model)
+            self.setCentralWidget(self.table)
+            #print(masv_df)
+                 
 
 
 app = QApplication(sys.argv)
