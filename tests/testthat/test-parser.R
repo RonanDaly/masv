0b3fd7ef1837998a6634955386d28dc033946385
CorwinAnsley <coreyansley@gmail.com>
2025-08-25 12:37:40 +0100

    updated tests

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -14,8 +14,24 @@ library(testthat)
 test_that("test parseExpressionSets", {
   #setwd('../..')
   #print(getwd())
-  e_set_list = parseExpressionSets(test_path("testdata", "masv1test.tsv"))
+  e_set_list = parseExpressionSets(test_path("testdata", "masv2test.tsv"))
   e_sets = e_set_list$e_sets
   eset = e_sets[[1]]
   expect_equal(names(eset@featureData@data)[1], "mz")
+})
+
+test_that("test parseMultiDataSet", {
+  multi = parseMultiDataSet(test_path("testdata", "masv2test.tsv"))
+  set = names(multi)[[1]]
+  print(set)
+  # Get data
+  data = assayData(multi[,set])
+  data = data[[1]]
+  data = data$exprs
+  #data_sets[[set]] = data
+  print('a')
+  features = list(rownames(data))[[1]]
+  feature1 = features[1]
+
+  expect_equal(feature1, "Feature1")
 })
